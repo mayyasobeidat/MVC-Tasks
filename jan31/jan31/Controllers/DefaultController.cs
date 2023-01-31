7433db93931139355507b348cb1709b944fa533b
15 - Mayyas Obeidat <mayyasobeidat@gmail.com>
2023-01-31 20:26:45 +0300

    new task

---
+++ jan31/jan31/Controllers/DefaultController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jan31.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+       
+            public ActionResult Index()
+            {
+                return Content("<p>Click on the image to download it:<p>" +
+                        "\r\n<a href=\"/img.jpg\" download>" +
+                        "\r\n  <img src=\"/img.jpg\" alt=\"Photographer\" " +
+                        "width=\"304\" height=\"142\">\r\n</a>");
+
+            }
+
+        public ActionResult about()
+        {
+            return Content(" <div style=\"text-align:center;\">\r\n    <h1>About Us</h1>\r\n    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, velit. Aut, eveniet deserunt culpa consequatur sunt et. In, perferendis! Nemo beatae corrupti dolorum doloremque vitae ut doloribus sequi accusantium adipisci!</p>\r\n    </div>");
+        }
+
+        public ActionResult contact()
+        {
+            return Content("   <div style=\"text-align:center;\">\r\n    <h1>Contact Us</h1>\r\n    <ul style=\"list-style-type: none;\">\r\n        <li>Phone : 0797383346</li>\r\n        <li>Email : mayyasobeidat@gmail.com</li>\r\n        <li>Address : Irbid</li>\r\n        <li>Instagram : @mayyasobeidatt</li>\r\n    </ul>\r\n    </div>");
+        }
+
+    }
+    }
---
+++ jan31/jan31/Controllers/MayyasController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jan31.Controllers
+{
+    public class MayyasController : Controller
+    {
+        // GET: Mayyas
+        public ActionResult Name()
+        {
+            return Content("<p>mayyas obeidat</p>\r\n");
+        }
+
+        public ActionResult Photo()
+        {
+            return Content("<img src=\"../img.jpg\"> width=\"100px\"");
+        }
+
+        public ActionResult Link()
+        {
+            return Content("<a href=\"https://www.instagram.com/mayyasobeidatt/\">Mayyas</a>");
+
+        }
+
+        public ActionResult table()
+        {
+            return Content(" <table>\r\n        <tr>\r\n            <td>instagram</td>\r\n            <td>facebook</td>\r\n            <td>linkedin</td>\r\n        </tr>\r\n        <tr>\r\n            <td><a href=\"https://www.instagram.com/mayyasobeidatt/\">Mayyas</a></td>\r\n            <td><a href=\"https://www.facebook.com/mayyasobeidat/\">Mayyas</a></td>\r\n            <td><a href=\"https://www.linkedin.com/in/mayyasobeidat/\">Mayyas</a></td>\r\n        </tr>\r\n    </table>");
+
+        }
+    }
+}
