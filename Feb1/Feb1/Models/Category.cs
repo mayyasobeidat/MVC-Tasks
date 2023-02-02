7974f700b3873864fdc60388942b2257129b4c7f
15 - Mayyas Obeidat <mayyasobeidat@gmail.com>
2023-02-02 21:25:16 +0300

    new task

--- Feb1/Feb1/Models/Category.cs
+++ Feb1/Feb1/Models/Category.cs
@@ -11,7 +11,8 @@ namespace Feb1.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Category
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,11 +20,13 @@ namespace Feb1.Models
         {
             this.Products = new HashSet<Product>();
         }
-    
+
         public int CategoryID { get; set; }
+        [Required]
         public string CategoryName { get; set; }
+        [Required]
         public string Description { get; set; }
-    
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Product> Products { get; set; }
     }
