678e869d2a1e6d542d46400757f14788343dc512
15 - Mayyas Obeidat <mayyasobeidat@gmail.com>
2023-02-06 16:59:09 +0300

    new task

---
+++ Feb-6/Feb-6/Controllers/InformationController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Feb_6.Models;
+
+namespace Feb_6.Controllers
+{
+    public class InformationController : Controller
+    {
+        private MVCEntities db = new MVCEntities();
+
+        // GET: Information
+        public ActionResult Index()
+        {
+            return View(db.Information.ToList());
+        }
+
+
+        public PartialViewResult LastOrder()
+        {
+            var lastOrder = db.Orders.OrderByDescending(x => x.OrderDate).FirstOrDefault();
+            return PartialView("_LastOrderView" , lastOrder);
+        }
+
+
+        // GET: Information/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Information information = db.Information.Find(id);
+            if (information == null)
+            {
+                return HttpNotFound();
+            }
+            return View(information);
+        }
+
+        // GET: Information/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Information/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,First_Name,Last_Name,E_mail,Phone,Age,Job_Title,Gender,Image,CV")] Information information)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Information.Add(information);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(information);
+        }
+
+        // GET: Information/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Information information = db.Information.Find(id);
+            if (information == null)
+            {
+                return HttpNotFound();
+            }
+            return View(information);
+        }
+
+        // POST: Information/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,First_Name,Last_Name,E_mail,Phone,Age,Job_Title,Gender,Image,CV")] Information information)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(information).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(information);
+        }
+
+        // GET: Information/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Information information = db.Information.Find(id);
+            if (information == null)
+            {
+                return HttpNotFound();
+            }
+            return View(information);
+        }
+
+        // POST: Information/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Information information = db.Information.Find(id);
+            db.Information.Remove(information);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
